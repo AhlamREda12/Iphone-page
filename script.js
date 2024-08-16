657493b7c4666d7ec218134818ec8104cea5269f
AhlamREda12 <168923423+AhlamREda12@users.noreply.github.com>
2024-08-17 00:29:57 +0300

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let img = document.querySelector(".img");
+let containar = document.querySelector(".containar");
+
+function phones(phone){
+    img.src=phone
+}
+function colors(color){
+    containar.style.background=color
+}
